229e886b33894fd8577c5b264ae8af4895181fdf
Igor Shapovalov <shapovalovii2000@gmail.com>
2019-08-20 18:06:22 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## How to run test: 
+
+* Use api-key `trueautomation api-key 3b404d60-7df0-4101-b755-7102e6cf070e` command
+ 
+* Run test
+
+```bash
+mvn -Dtest=NonexistentTALocatorTest test
+mvn -Dtest=TakenFromAnotherPageTALocatorTest test
+
+```
---
+++ src/test/java/TakenFromAnotherPageTALocatorTest.java
@@ -0,0 +1,33 @@
+import io.trueautomation.client.driver.TrueAutomationDriver;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static io.trueautomation.client.TrueAutomationHelper.byTa;
+
+public class TakenFromAnotherPageTALocatorTest {
+    private WebDriver driver;
+
+    @BeforeTest
+    public void beforeTest() {
+        driver = new TrueAutomationDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void exampleTest() {
+        driver.get("https://trueautomation.io");
+
+        driver.findElement(byTa("Google:Translate")).click();
+
+
+    }
+
+    @AfterTest
+    public void afterTest() {
+        driver.quit();
+    }
+}
